ebc5169996641fe49c07371608e8f48e5770543e
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-05-03 15:36:54 -0400

    add gym shoe test

--- backpack_test.rb
+++ backpack_test.rb
@@ -43,8 +43,16 @@ class BackpackTest < MiniTest::Test
   end
 
 
-  ###### TESTS ABOUT GYM SHOES... wait, where are the tests about gym shoes? #####
-  # is something missing here?
+  def test_that_packs_gym_shoes
+    ['monday','thursday'].each do |day|
+      backpack = build(:backpack, day_of_week: day)
+      assert(backpack.items.include?('gym shoes'))
+    end
+  end
+
+  def test_that_packs_gym_shoes
+
+  end
 
 
   ##### TESTS ABOUT PRINTING THE LIST #####
